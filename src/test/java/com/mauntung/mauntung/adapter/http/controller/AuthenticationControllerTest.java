d86151ddabd85c264bfb7b8f5ad3d2c406b846d9
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-12-21 14:24:23 +0700

    refactor AuthenticationControllerTest

--- src/test/java/com/mauntung/mauntung/adapter/http/controller/AuthenticationControllerTest.java
+++ src/test/java/com/mauntung/mauntung/adapter/http/controller/AuthenticationControllerTest.java
@@ -1,5 +1,7 @@
 package com.mauntung.mauntung.adapter.http.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mauntung.mauntung.application.exception.UserWithSpecifiedEmailExistedException;
 import com.mauntung.mauntung.application.port.customer.CustomerRegisterResponse;
 import com.mauntung.mauntung.application.port.customer.CustomerRegisterUseCase;
@@ -13,6 +15,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Map;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,6 +24,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(AuthenticationController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class AuthenticationControllerTest {
+    private static final String MERCHANT_REGISTER_URL = "/merchant/auth/register";
+    private static final String CUSTOMER_REGISTER_URL = "/customer/auth/register";
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
+
     @Autowired
     private MockMvc mvc;
 
@@ -29,16 +37,33 @@ class AuthenticationControllerTest {
     @MockBean
     private CustomerRegisterUseCase customerRegisterUseCase;
 
+    private static String prepareMerchantRegisterRequestBody(String merchantName, String email, String password) throws JsonProcessingException {
+        return jsonMapper.writeValueAsString(Map.of(
+            "merchantName", merchantName,
+            "email", email,
+            "password", password
+        ));
+    }
+
+    private static String prepareCustomerRegisterRequestBody(String fullName, String email, String password) throws JsonProcessingException {
+        return jsonMapper.writeValueAsString(Map.of(
+            "fullName", fullName,
+            "email", email,
+            "password", password
+        ));
+    }
+
     @Test
     void givenValidEmail_merchantRegister_shouldReturnCreatedStatus() throws Exception {
-        String requestBody = "{\n" +
-            "    \"merchantName\": \"KopiQue\",\n" +
-            "    \"email\": \"kopique@merchant.com\",\n" +
-            "    \"password\": \"password123\"\n" +
-            "}";
+        String merchantName = "KopiQue";
+        String email = "kopique@merchant.com";
+        String password = "password123";
+        String requestBody = prepareMerchantRegisterRequestBody(merchantName, email, password);
+
         when(merchantRegisterUseCase.apply(any())).thenReturn(new MerchantRegisterResponse(1L, 1L, "KopiQue"));
+
         mvc.perform(
-                post("/merchant/auth/register")
+                post(MERCHANT_REGISTER_URL)
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -48,14 +73,15 @@ class AuthenticationControllerTest {
 
     @Test
     void givenMalformedEmail_merchantRegister_shouldReturnBadRequestStatus() throws Exception {
-        String requestBody = "{\n" +
-            "    \"merchantName\": \"KopiQue\",\n" +
-            "    \"email\": \"kopique@merchant\",\n" +
-            "    \"password\": \"password123\"\n" +
-            "}";
+        String merchantName = "KopiQue";
+        String email = "kopique";
+        String password = "password123";
+        String requestBody = prepareMerchantRegisterRequestBody(merchantName, email, password);
+
         when(merchantRegisterUseCase.apply(any())).thenThrow(new IllegalArgumentException("Invalid email"));
+
         mvc.perform(
-                post("/merchant/auth/register")
+                post(MERCHANT_REGISTER_URL)
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -65,14 +91,15 @@ class AuthenticationControllerTest {
 
     @Test
     void givenExistedEmail_merchantRegister_shouldReturnBadRequestStatus() throws Exception {
-        String requestBody = "{\n" +
-            "    \"merchantName\": \"KopiQue\",\n" +
-            "    \"email\": \"kopique@merchant.com\",\n" +
-            "    \"password\": \"password123\"\n" +
-            "}";
+        String merchantName = "KopiQue";
+        String email = "kopique@merchant.com";
+        String password = "password123";
+        String requestBody = prepareMerchantRegisterRequestBody(merchantName, email, password);
+
         when(merchantRegisterUseCase.apply(any())).thenThrow(new UserWithSpecifiedEmailExistedException());
+
         mvc.perform(
-                post("/merchant/auth/register")
+                post(MERCHANT_REGISTER_URL)
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -82,14 +109,15 @@ class AuthenticationControllerTest {
 
     @Test
     void givenValidEmail_customerRegister_shouldReturnCreatedStatus() throws Exception {
-        String requestBody = "{\n" +
-            "    \"fullName\": \"Joomla\",\n" +
-            "    \"email\": \"joomla@customer.com\",\n" +
-            "    \"password\": \"password123\"\n" +
-            "}";
+        String fullName = "Joomla";
+        String email = "joomla@customer.com";
+        String password = "password123";
+        String requestBody = prepareCustomerRegisterRequestBody(fullName, email, password);
+
         when(customerRegisterUseCase.apply(any())).thenReturn(new CustomerRegisterResponse(1L, 1L, "Joomla"));
+
         mvc.perform(
-                post("/customer/auth/register")
+                post(CUSTOMER_REGISTER_URL)
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -99,14 +127,15 @@ class AuthenticationControllerTest {
 
     @Test
     void givenMalformedEmail_customerRegister_shouldReturnBadRequestStatus() throws Exception {
-        String requestBody = "{\n" +
-            "    \"fullName\": \"Joomla\",\n" +
-            "    \"email\": \"joomlacustomer.com\",\n" +
-            "    \"password\": \"password123\"\n" +
-            "}";
+        String fullName = "Joomla";
+        String email = "joomla";
+        String password = "password123";
+        String requestBody = prepareCustomerRegisterRequestBody(fullName, email, password);
+
         when(customerRegisterUseCase.apply(any())).thenThrow(new IllegalArgumentException("Invalid email"));
+
         mvc.perform(
-                post("/customer/auth/register")
+                post(CUSTOMER_REGISTER_URL)
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON)
             )
@@ -116,14 +145,15 @@ class AuthenticationControllerTest {
 
     @Test
     void givenExistedEmail_customerRegister_shouldReturnBadRequestStatus() throws Exception {
-        String requestBody = "{\n" +
-            "    \"fullName\": \"Joomla\",\n" +
-            "    \"email\": \"joomla@customer.com\",\n" +
-            "    \"password\": \"password123\"\n" +
-            "}";
+        String fullName = "Joomla";
+        String email = "joomla@customer.com";
+        String password = "password123";
+        String requestBody = prepareCustomerRegisterRequestBody(fullName, email, password);
+
         when(customerRegisterUseCase.apply(any())).thenThrow(new UserWithSpecifiedEmailExistedException());
+
         mvc.perform(
-                post("/customer/auth/register")
+                post(CUSTOMER_REGISTER_URL)
                     .content(requestBody)
                     .contentType(MediaType.APPLICATION_JSON)
             )
