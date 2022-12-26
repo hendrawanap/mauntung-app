8a247954a67f0be9d3e245c7693303b0972fc434
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-12-26 11:52:02 +0700

    implements custom enum validator annotation

---
+++ src/main/java/com/mauntung/mauntung/common/validator/ValueOfEnumValidator.java
@@ -0,0 +1,27 @@
+package com.mauntung.mauntung.common.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
+    private List<String> acceptedValues;
+
+    @Override
+    public void initialize(ValueOfEnum constraintAnnotation) {
+        acceptedValues = Stream.of(constraintAnnotation.enumClass().getEnumConstants())
+            .map(Enum::toString)
+            .map(String::toLowerCase)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        if (value == null)
+            return true;
+
+        return acceptedValues.contains(value.toString().toLowerCase());
+    }
+}
