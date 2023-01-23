61f7e3ac2684999ef4a3e18af2a11db315d729b7
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2023-01-23 14:52:16 +0700

    implements 'create default membership' in MerchantRegisterService

--- src/main/java/com/mauntung/mauntung/application/service/MerchantRegisterService.java
+++ src/main/java/com/mauntung/mauntung/application/service/MerchantRegisterService.java
@@ -1,11 +1,13 @@
 package com.mauntung.mauntung.application.service;
 
 import com.mauntung.mauntung.application.exception.UserWithSpecifiedEmailExistedException;
+import com.mauntung.mauntung.application.port.membership.MembershipRepository;
 import com.mauntung.mauntung.application.port.merchant.MerchantRegisterCommand;
 import com.mauntung.mauntung.application.port.merchant.MerchantRegisterUseCase;
 import com.mauntung.mauntung.application.port.merchant.MerchantRegisterResponse;
 import com.mauntung.mauntung.application.port.merchant.MerchantRepository;
 import com.mauntung.mauntung.application.port.user.UserRepository;
+import com.mauntung.mauntung.domain.model.membership.*;
 import com.mauntung.mauntung.domain.model.merchant.Merchant;
 import com.mauntung.mauntung.domain.model.merchant.MerchantFactory;
 import com.mauntung.mauntung.domain.model.merchant.MerchantFactoryImpl;
@@ -18,15 +20,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class MerchantRegisterService implements MerchantRegisterUseCase {
     private final UserRepository userRepository;
     private final MerchantRepository merchantRepository;
+    private final MembershipRepository membershipRepository;
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private final UserFactory userFactory = new UserFactoryImpl();
     private final MerchantFactory merchantFactory = new MerchantFactoryImpl();
+    private final StampMembershipFactory stampMembershipFactory = new StampMembershipFactoryImpl();
 
     @Override
     public MerchantRegisterResponse apply(MerchantRegisterCommand command) {
@@ -41,11 +46,24 @@ public class MerchantRegisterService implements MerchantRegisterUseCase {
         Merchant merchant = createMerchant(command.getMerchantName());
         Long merchantId = saveMerchantAndGetId(merchant, userId);
 
+        Merchant savedMerchant = createMerchant(merchant, merchantId);
+        Membership defaultMembership = createDefaultMembership(savedMerchant);
+        saveDefaultMembership(defaultMembership);
+
         return buildResponse(userId, merchantId, merchant.getName());
     }
 
     private MerchantRegisterResponse buildResponse(long userId, long merchantId, String merchantName) {
         return new MerchantRegisterResponse(userId, merchantId, merchantName);
+    }
+
+    private void saveDefaultMembership(Membership membership) {
+        membershipRepository.save(membership).orElseThrow(() -> new RuntimeException("Failed to save default membership"));
+    }
+
+    private Membership createDefaultMembership(Merchant merchant) {
+        StampRules defaultRules = new StampRules(90, 90, 10);
+        return stampMembershipFactory.builder("", merchant, Set.of(), new Date(), defaultRules, false).build();
     }
 
     private Long saveMerchantAndGetId(Merchant merchant, Long userId) {
@@ -66,5 +84,11 @@ public class MerchantRegisterService implements MerchantRegisterUseCase {
 
     private Merchant createMerchant(String merchantName) throws IllegalArgumentException {
         return merchantFactory.builder(merchantName, new Date()).build();
+    }
+
+    private Merchant createMerchant(Merchant merchant, long merchantId) {
+        return merchantFactory.builder(merchant.getName(), merchant.getCreatedAt())
+            .id(merchantId)
+            .build();
     }
 }
--- src/test/java/com/mauntung/mauntung/application/service/MerchantRegisterServiceTest.java
+++ src/test/java/com/mauntung/mauntung/application/service/MerchantRegisterServiceTest.java
@@ -1,6 +1,7 @@
 package com.mauntung.mauntung.application.service;
 
 import com.mauntung.mauntung.application.exception.UserWithSpecifiedEmailExistedException;
+import com.mauntung.mauntung.application.port.membership.MembershipRepository;
 import com.mauntung.mauntung.application.port.merchant.MerchantRegisterCommand;
 import com.mauntung.mauntung.application.port.merchant.MerchantRegisterUseCase;
 import com.mauntung.mauntung.application.port.merchant.MerchantRepository;
@@ -18,6 +19,7 @@ import static org.mockito.Mockito.*;
 class MerchantRegisterServiceTest {
     private static UserRepository userRepository;
     private static MerchantRepository merchantRepository;
+    private static MembershipRepository membershipRepository;
     private static User user;
     private static Merchant merchant;
     private static String email;
@@ -28,6 +30,7 @@ class MerchantRegisterServiceTest {
     static void beforeAll() {
         userRepository = mock(UserRepository.class);
         merchantRepository = mock(MerchantRepository.class);
+        membershipRepository = mock(MembershipRepository.class);
         user = mock(User.class);
         merchant = mock(Merchant.class);
         email = "merchant1@mail.com";
@@ -41,7 +44,7 @@ class MerchantRegisterServiceTest {
         MerchantRepository merchantRepository = mock(MerchantRepository.class);
 
         MerchantRegisterCommand command = new MerchantRegisterCommand(email, password, merchantName);
-        MerchantRegisterUseCase service = new MerchantRegisterService(userRepository, merchantRepository);
+        MerchantRegisterUseCase service = new MerchantRegisterService(userRepository, merchantRepository, membershipRepository);
 
         assertThrows(UserWithSpecifiedEmailExistedException.class, () -> service.apply(command));
     }
@@ -50,13 +53,15 @@ class MerchantRegisterServiceTest {
     void givenNonExistedEmail_apply_shouldReturnSuccessResponse() {
         Long userId = 1L;
         Long merchantId = 1L;
+        Long membershipId = 1L;
 
         when(merchant.getName()).thenReturn(merchantName);
         when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
         when(userRepository.save(any())).thenReturn(Optional.of(userId));
         when(merchantRepository.save(any(), any())).thenReturn(Optional.of(merchantId));
+        when(membershipRepository.save(any())).thenReturn(Optional.of(membershipId));
 
-        MerchantRegisterUseCase service = new MerchantRegisterService(userRepository, merchantRepository);
+        MerchantRegisterUseCase service = new MerchantRegisterService(userRepository, merchantRepository, membershipRepository);
         MerchantRegisterCommand command = new MerchantRegisterCommand(email, password, merchantName);
 
         assertNotNull(service.apply(command));
