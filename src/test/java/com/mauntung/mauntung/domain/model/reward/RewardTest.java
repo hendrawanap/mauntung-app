41cc0d29af39dda3f85b0fb075e9b69f21d744a9
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2023-02-12 14:16:30 +0700

    refactor RewardTest

--- src/test/java/com/mauntung/mauntung/domain/model/reward/RewardTest.java
+++ src/test/java/com/mauntung/mauntung/domain/model/reward/RewardTest.java
@@ -1,29 +1,29 @@
 package com.mauntung.mauntung.domain.model.reward;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RewardTest {
-    static final RewardFactory rewardFactory = new RewardFactoryImpl();
-    static final String name = "name";
-    static final String description = "description";
-    static final String termsCondition = "termsCondition";
-    static final int cost = 10;
-    static final int stock = 10;
+    static RewardFactory rewardFactory;
+    static String name;
+    static String description;
+    static String termsCondition;
+    static int cost;
+    static int stock;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @BeforeAll
+    static void beforeAll() {
+        rewardFactory = new RewardFactoryImpl();
+        name = "name";
+        description = "description";
+        termsCondition = "termsCondition";
+        cost = 10;
+        stock = 10;
     }
 
     @Test
@@ -31,55 +31,61 @@ class RewardTest {
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost)
             .stock(stock)
             .build();
+
         assertTrue(reward.isClaimable());
     }
 
     @Test
     void givenNullStockAndValidPeriods_isClaimable_shouldReturnTrue() {
-        Instant now = Instant.now();
+        Date now = new Date();
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost)
-            .startPeriod(new Date(now.minus(1, ChronoUnit.DAYS).toEpochMilli()))
-            .endPeriod(new Date(now.plus(10, ChronoUnit.DAYS).toEpochMilli()))
+            .startPeriod(createPastDate(1, now))
+            .endPeriod(createFutureDate(10, now))
             .build();
+
         assertTrue(reward.isClaimable());
     }
 
     @Test
     void givenNullPeriodsAndNullStock_isClaimable_shouldReturnTrue() {
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost).build();
+
         assertTrue(reward.isClaimable());
     }
 
     @Test
     void givenStartPeriodIsNowAndEndPeriodAfterNow_isClaimable_shouldReturnTrue() {
-        Instant now = Instant.now();
+        Date now = new Date();
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost)
             .stock(stock)
-            .startPeriod(new Date(now.toEpochMilli()))
-            .endPeriod(new Date(now.plus(10, ChronoUnit.DAYS).toEpochMilli()))
+            .startPeriod(now)
+            .endPeriod(createFutureDate(10, now))
             .build();
+
         assertTrue(reward.isClaimable());
     }
 
     @Test
     void givenStartPeriodAndEndPeriodAreAfterNow_isClaimable_shouldReturnFalse() {
-        Instant now = Instant.now();
+        Date now = new Date();
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost)
             .stock(stock)
-            .startPeriod(new Date(now.plus(1, ChronoUnit.DAYS).toEpochMilli()))
-            .endPeriod(new Date(now.plus(10, ChronoUnit.DAYS).toEpochMilli()))
+            .startPeriod(createFutureDate(1, now))
+            .endPeriod(createFutureDate(10, now))
             .build();
+
         assertFalse(reward.isClaimable());
     }
 
     @Test
     void givenStartPeriodIsBeforeNowAndEndPeriodIsNow_isClaimable_shouldReturnFalse() {
-        Instant now = Instant.now();
+        Date now = new Date();
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost)
             .stock(stock)
-            .startPeriod(new Date(now.minus(10, ChronoUnit.DAYS).toEpochMilli()))
-            .endPeriod(new Date(now.toEpochMilli()))
+            .startPeriod(createPastDate(10, now))
+            .endPeriod(now)
             .build();
+
         assertFalse(reward.isClaimable());
     }
 
@@ -88,6 +94,15 @@ class RewardTest {
         Reward reward = rewardFactory.builder(name, description, termsCondition, cost)
             .stock(0)
             .build();
+
         assertFalse(reward.isClaimable());
+    }
+
+    Date createPastDate(int daysPassed, Date dateSince) {
+        return new Date(dateSince.toInstant().minus(daysPassed, ChronoUnit.DAYS).toEpochMilli());
+    }
+
+    Date createFutureDate(int daysInFuture, Date dateSince) {
+        return new Date(dateSince.toInstant().plus(daysInFuture, ChronoUnit.DAYS).toEpochMilli());
     }
 }
